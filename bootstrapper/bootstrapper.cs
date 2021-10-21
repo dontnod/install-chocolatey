2f398f5c4b7d31a94b73bcbd1367d2e8373d4b72
Sam Hocevar <sam@hocevar.net>
2021-10-21 08:19:12 +0200

    Rework bootstrapper code for clarity

--- bootstrapper/bootstrapper.cs
+++ bootstrapper/bootstrapper.cs
@@ -12,36 +12,45 @@ namespace bootstrapper
         static void Main(string[] args)
         {
             // If not running with elevated privileges, try to acquire them
+            if (!IsAdministrator())
+            {
+                RelaunchAsAdministrator();
+                return;
+            }
+
+            // Set this environment variable to a sensible default value, otherwise chocolatey.lib will use the executable location
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ChocolateyInstall")))
+                Environment.SetEnvironmentVariable("ChocolateyInstall", @"C:\ProgramData\chocolatey");
+
+            var choco = Lets.GetChocolatey();
+            choco.SetCustomLogging(new NullLog());
+
+            choco.Set(conf => conf.CommandName = "upgrade");
+            choco.RunConsole(new string[] { "-y", "--force", "chocolatey" });
+            choco.Set(conf => conf.CommandName = "upgrade");
+            choco.RunConsole(new string[] { "-y", "--force", "chocolateygui" });
+        }
+
+        static bool IsAdministrator()
+        {
             var identity = WindowsIdentity.GetCurrent();
             var principal = new WindowsPrincipal(identity);
-            if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        static void RelaunchAsAdministrator()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = Assembly.GetExecutingAssembly().Location;
+            p.StartInfo.Verb = "runas"; // elevated privileges
+            p.StartInfo.UseShellExecute = true;
+            try
             {
-                Process p = new Process();
-                p.StartInfo.FileName = Assembly.GetExecutingAssembly().Location;
-                p.StartInfo.Verb = "runas"; // elevated privileges
-                p.StartInfo.UseShellExecute = true;
-                try
-                {
-                    p.Start();
-                }
-                catch
-                {
-                    Console.WriteLine("Error: must be run as administrator");
-                }
+                p.Start();
             }
-            else
+            catch
             {
-                // Set this environment variable to a sensible default value, otherwise chocolatey.lib will use the executable location
-                if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ChocolateyInstall")))
-                    Environment.SetEnvironmentVariable("ChocolateyInstall", @"C:\ProgramData\chocolatey");
-
-                var choco = Lets.GetChocolatey();
-                choco.SetCustomLogging(new NullLog());
-
-                choco.Set(conf => conf.CommandName = "upgrade");
-                choco.RunConsole(new string[] { "-y", "--force", "chocolatey" });
-                choco.Set(conf => conf.CommandName = "upgrade");
-                choco.RunConsole(new string[] { "-y", "--force", "chocolateygui" });
+                Console.WriteLine("Error: must be run as administrator");
             }
         }
     }
